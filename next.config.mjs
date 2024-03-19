a176481257efa8007081f5b1062307a7a8b646e5
oknotok1 <72703102+oknotok1@users.noreply.github.com>
2024-03-19 14:27:01 +0800

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "d10sa4fpz3p23y.cloudfront.net",
+        port: "",
+        pathname: "/public/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cinch-sg-production.s3.ap-southeast-1.amazonaws.com",
+        port: "",
+        pathname: "/public/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
