c947ccbd50a0dda2c364e73a35b8249540704d5b
oknotok1 <72703102+oknotok1@users.noreply.github.com>
2025-01-25 02:16:47 +0800

    Moved instructions to README.md

--- app/employees-of-the-month/page.tsx
+++ app/employees-of-the-month/page.tsx
@@ -1,27 +1,3 @@
-/**
- * API Documentation: https://randomuser.me/documentation
- *
- * Instructions:
- * 1. Fetch 20 random users from the Random User Generator API
- * 3. Generate the Typescript types for the API response & use it to Create an EmployeeCard component that displays the following information about each user:
- *   a. Store the API response in a file in `/types/RandomPerson.ts`
- * 4. Following the design in `/public/Employees Of The Month Page, create a page that displays the following information about each user:
- *   a. Name & Title (name.first, name.last, name.title)
- *     i. Format: "firstName lastName (title)"
- *   b. An avatar image (picture.large) with a status indicator (online/offline).
- *     i. Create a random boolean value to determine if the user is online or offline.
- *   c. Location (city, state)
- *   d. Email address (email) with the Email MUI Icon that when clicked, opens the user's email client
- *   e. Date of birth in DD MMM YYYY format (dob.date), and age (dob.age)
- *   f. The time zone offset (timezone.offset) and description (timezone.description) in UTC±X format with the AccessTime MUI Icon
- * 5. Every line should have a maximum of 1 line of text with an ellipsis for overflow
- *   a. Add a tooltip to show the full text on hover
- * 6. The page layout should be responsive with:
- *   a. A maximum of 4 cards per row on large screens
- *   b. A maximum of 2 cards per row on medium screens
- *   c. A single card per row on small screens
- */
-
 import type { RandomUserApiResponse } from "@/types/RandomPerson";
 import EmployeeCard from "./components/EmployeeCard";
 
@@ -33,6 +9,9 @@ export default async function People() {
         These are our top performing employees of the month, and we would like
         to recognize them.
       </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-2">
+        {/* Iterate through the objects and render the EmployeeCard component here */}
+      </div>
     </div>
   );
 }
--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,33 +1,7 @@
-import recipes from "@/data/recipes";
 import RecipeCard from "@/app/recipes/components/RecipeCard";
 import type { Recipe } from "@/types/Recipe";
 
-const generateRandomRecipeIds = (count: number) => {
-  const randomRecipeIds = [];
-  for (let i = 0; i < count; i++) {
-    const randomIndex = Math.floor(Math.random() * 100000);
-    randomRecipeIds.push(randomIndex);
-  }
-  return randomRecipeIds;
-};
-
 export default async function Recipes() {
-  // const baseUrl = "https://api.spoonacular.com";
-  // const path = "/recipes/informationBulk";
-  // const apiKey = process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY;
-  // const recipeIds = [
-  //   123456, 111333, 555226, 321556, 234732, 716429, 715538, 715539, 715540,
-  // ];
-  // const url = `${baseUrl}${path}?ids=${recipeIds.join(",")}&apiKey=${apiKey}`;
-
-  // const randomRecipeIds = generateRandomRecipeIds(9);
-  // const url = `${baseUrl}${path}?ids=${randomRecipeIds.join(
-  //   ","
-  // )}&apiKey=${apiKey}`;
-
-  // const data = await fetch(url);
-  // const recipes = await data.json();
-
   return (
     <div className="max-w-[1400px] mx-auto p-4 lg:p-8 flex flex-col gap-4">
       <h1 className="text-4xl">Spoonacular Recipes</h1>
@@ -35,9 +9,7 @@ export default async function Recipes() {
         Here are some delicious recipes from Spoonacular for you to explore.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mt-2">
-        {recipes.map((recipe: Recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
+        {/* Iterate through the objects and render the RecipeCard component here */}
       </div>
     </div>
   );
